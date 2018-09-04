28a88d29c33d7086427ce003a677865c0229e09c
leonardo.costa <leonardo.costa@EST41250.mctic.gov.br>
2018-09-04 15:32:26 -0300

    Configuracao da rota das paginas

--- WebContent/gerenciar/gerenciamentoFretes/GerenciarFrete.module.js
+++ WebContent/gerenciar/gerenciamentoFretes/GerenciarFrete.module.js
@@ -3,7 +3,7 @@ var app = angular.module('GerenciarFrete.module', [])
 app.controller('listartarifas', function($scope, $http, $location) {
 
 	$scope.listartarifas = function() {
-		var url = "/POC-Sistema-Logistica-0.0.1-SNAPSHOT/rest/tarifa/listartarifas";
+		var url = "/sislogis/rest/tarifa/listartarifas";
  
 		$http.get(url).then(function(response) {
 			$scope.listaTarifa = response.data;
